5ef202d5705bbaec0892dcafb854dda87ef176eb
Juan Cantero <42438915+juanqui1989@users.noreply.github.com>
2019-04-08 21:37:03 -0300

    Update Complejo.java

--- src/Complejo.java
+++ src/Complejo.java
@@ -31,4 +31,27 @@ public class Complejo {
         imaginario = imaginario + complejo.imaginario;
 
     }
+     public void resta(Complejo complejo){
+        this.real = this.real - complejo.real;
+        this.imaginario = this.imaginario - complejo.imaginario;
+    }
+
+    public float modulo() {
+        return (float)Math.sqrt(Math.pow(this.real,2) + Math.pow(this.imaginario,2) );
+    }
+
+    public float fase() {
+        return (float)Math.atan(this.imaginario / this.real)
+    }
+
+    public float conjugado() {
+        return this.real - this.imaginario;
+    }
+
+    public void multiplicar(Complejo complejo) {
+        //float multiplicacion;
+        //multiplicacion = this.real*(complejo.real + complejo.imaginario)
+        return;
+    }
+    
 }
