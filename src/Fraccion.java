5f357efb2f2cafcf0ab19625f321804e29ccc741
juanqui1989 <androideva02@gmail.com>
2019-04-12 11:11:35 -0300

    algo es algo

---
+++ src/Fraccion.java
@@ -0,0 +1,22 @@
+public class Fraccion {
+  int numerator = 0;
+  int denominator = 0;
+
+  public void setNumerator(int numerator) {
+    this.numerator = numerator;
+  }
+
+  public void setDenominator(int denominator) {
+    this.denominator = numerator;
+  }
+
+  public int getNumerator() {
+    return this.numerator;
+  }
+
+  public int getDenominator() {
+    return this.denominator;
+  }
+
+
+}
